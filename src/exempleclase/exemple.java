b000534b4f0a998294e79312f7734f1476b945a6
𝑱𝒂𝒏 𝑸𝒖𝒆𝒓𝒂𝒍𝒕 <55952925+jaanque@users.noreply.github.com>
2024-11-11 19:20:34 +0100

    hola mundo en java

--- src/exempleclase/exemple.java
+++ src/exempleclase/exemple.java
@@ -3,8 +3,7 @@ package exempleclase;
 public class exemple {
 	public static void main(String[] args) {
 		
-		System.out.println("Hola");
-		System.out.println("Adios");
+		System.out.println("Hola mundo");
 	}
 
 }
