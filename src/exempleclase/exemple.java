6197c811083308eb70cc320953e6538e4bd3b1f3
𝑱𝒂𝒏 𝑸𝒖𝒆𝒓𝒂𝒍𝒕 <55952925+jaanque@users.noreply.github.com>
2024-11-11 19:24:26 +0100

    Revert "update"
    This reverts commit c96b86fb050e3f922766a1a6205dd3b1d698d67c.

--- src/exempleclase/exemple.java
+++ src/exempleclase/exemple.java
@@ -3,7 +3,7 @@ package exempleclase;
 public class exemple {
 	public static void main(String[] args) {
 		
-		// Hola y adios en java
+		// Hola y adios
 		System.out.println("Hola");
 		System.out.println("Adios");
 	}
